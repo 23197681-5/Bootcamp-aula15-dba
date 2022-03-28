5190119018efeebff4c98602a0e8164e2f21f5db
Willian Alberto Lauber <wlauber@pm.me>
2022-03-28 15:51:17 -0300

    refactor, move settings to a json file

--- WebApplication1/Controllers/TestController.cs
+++ WebApplication1/Controllers/TestController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
@@ -13,35 +14,48 @@ namespace WebApplication1.Controllers
     [Route("[controller]")]
     public class TestController : ControllerBase
     {
+        public TestController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        private IConfiguration Configuration;
+        private readonly IConfiguration _configuration;
+
         [HttpGet]
         public IActionResult Get()
         {
             try
             {
-                var connectionString =
-            "Server=127.0.0.1;Database=Bootcamp;User Id=sa;Password=yourStrong(!)Password;";
-                var connection = new SqlConnection(connectionString);
-                connection.Open();
-                var cmdText = "SELECT * FROM usuario";
-                var sqlCommand = new SqlCommand(cmdText, connection);
-                var usuarios = new List<Usuario>();
-                
-                using (var reader = sqlCommand.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        var usuario = new Usuario()
-                        {
-                            Id = Convert.ToInt32(reader["Id"]),
-                            Idade = Convert.ToInt32(reader["Idade"]),
-                            Nome = Convert.ToString(reader["Nome"]),
-                        };
+                var connectionString = _configuration.GetConnectionString("DevConnection");
 
-                        usuarios.Add(usuario);
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    var cmdText = "SELECT * FROM users";
+                    var sqlCommand = new SqlCommand(cmdText, connection);
+                    var users = new List<Usuario>();
+
+                    using (var reader = sqlCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var user = new Usuario()
+                            {
+                                Id = Convert.ToInt32(reader["Id"]),
+                                Idade = Convert.ToInt32(reader["Age"]),
+                                Nome = Convert.ToString(reader["Name"]),
+                            };
+
+                            users.Add(user);
+                        }
                     }
+
+                    // connection.Close();
+                    // connection.Dispose();
+                    return Ok(users.OrderByDescending(x => x.Nome));
                 }
 
-                return Ok(usuarios);
             }
             catch (Exception)
             {
